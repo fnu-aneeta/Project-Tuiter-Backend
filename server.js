72e8325af46f4fa159b5ccf20231b6709631b869
aneeta-f <aneeta.ochani@hotmail.com>
2022-04-27 16:56:31 -0400

    pushing changes in server.js

--- server.js
+++ server.js
@@ -60,7 +60,6 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
         ["http://localhost:3000"]);
-        //["https://aneeta-jobportal.web.app"]);
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
